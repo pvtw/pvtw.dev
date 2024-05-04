30e85b67a62182ce9aa2f0c600c9c88186929549
pvtw <patrick@pvtw.dev>
2024-05-04 17:15:19 +0200

    Home and about page + layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,105 @@
+@props([
+    "title" => "Patrick van 't Wout",
+    "description" => "Hi there! My name is Patrick van 't Wout and this is my mystical world of code and hacking.",
+    "image" => config('app.url') . "/images/og-image.png",
+    "fixed" => false,
+])
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ $title }} - {{ config('app.name') }}</title>
+
+        <meta name="title" content="{{ $title }}">
+        <meta name="description" content="{{ $description }}">
+
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ config('app.url') }}">
+        <meta property="og:title" content="{{ $title }}">
+        <meta property="og:description" content="{{ $description }}">
+        <meta property="og:image" content="{{ $image }}">
+
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="{{ config('app.url') }}">
+        <meta property="twitter:title" content="{{ $title }}">
+        <meta property="twitter:description" content="{{ $description }}">
+        <meta property="twitter:image" content="{{ $image }}">
+
+        <link rel="shortcut icon" href="/images/favicons/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="64x64" href="/images/favicons/favicon-64x64.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
+        <link rel="manifest" href="/images/favicons/site.webmanifest">
+        
+        <meta name="theme-color" content="#5c037f">
+        <meta name="color-scheme" content="light dark">
+
+        @livewireStyles
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="antialiased bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+        <div id="page" class="flex flex-col min-h-screen">
+            @php
+                $header_position = $fixed ? "fixed" : "sticky";
+            @endphp
+            <header class="bg-primary text-white flex flex-col sm:flex-row items-start sm:items-center justify-between {{ $header_position }} top-0 w-full shadow z-[9001]"
+                @keydown.window.escape="closeIfMobile"
+                x-trap.inert.noscroll="window.innerWidth < 640 && open"
+                x-data="{
+                    open: window.innerWidth >= 640,
+                    toggle() {
+                        this.open = ! this.open
+                    },
+                    closeIfMobile() {
+                        if (window.innerWidth < 640 && this.open) {
+                            this.open = false
+                        }
+                    },
+                }">
+                <a href="#content" class="block absolute top-0 left-4 bg-orange-500 text-black underline font-bold px-4 py-2 rounded-b shadow transition-transform duration-200 -translate-y-16 focus-visible:translate-y-0">Skip to content</a>
+                <div class="sm:w-auto w-full h-16 flex items-center justify-between px-2">
+                    <a href="{{ route('home') }}" class="block text-3xl font-bold p-2">Pvtw</a>
+                    
+                    <button class="block sm:hidden p-2" @click.prevent="toggle" :aria-expanded="open" aria-controls="primary-navigation">
+                        <span class="sr-only">Toggle menu</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8" aria-hidden="true" x-cloak x-show="! open">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8" aria-hidden="true" x-cloak x-show="open">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+                <nav id="primary-navigation" class="w-full sm:w-auto h-[calc(100vh-4rem)] sm:h-auto overflow-y-auto sm:overflow-y-visible sm:mr-2 p-2 sm:p-0" @click.away="closeIfMobile" x-cloak x-show="open">
+                    <ul class="flex flex-col sm:flex-row sm:space-x-2 sm:space-y-0 space-y-2">
+                        <li>
+                            <a
+                                href="{{ route('about') }}"
+                                class="block [&[aria-current='page']]:text-fuchsia-500 text-xl font-bold px-4 pt-2 pb-1 border-b-4 border-transparent hover:border-fuchsia-500 transition-colors duration-200"
+                                aria-current="{{ request()->is('about-me') ? 'page' : 'false' }}">
+                                About Me
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main id="content" class="flex-1">
+                {{ $slot }}
+            </main>
+            <footer class="bg-gray-950 text-white">
+                <div class="max-w-md mx-auto p-4 text-sm text-center">
+                    <p>
+                        My website is <a href="https://github.com/pvtw/pvtw.dev" class="text-blue-500 hover:underline">open source</a>.
+                        Feel free to contribute if you find a bug or have suggestions to improve my website.
+                    </p>
+                    <p class="mt-2">Copyright &copy; {{ now()->year }} - Patrick van 't Wout</p>
+                </div>
+            </footer>
+        </div>
+
+        @livewireScripts
+    </body>
+</html>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,149 @@
+<x-app-layout title="About Patrick van 't Wout" fixed="true">
+    <section class="relative min-h-screen flex items-center justify-center">
+        <div>
+            <h1 class="text-3xl md:text-5xl text-primary text-center font-bold dark:text-white">Patrick van 't Wout</h1>
+            <h2 class="text-lg md:text-xl text-gray-600 text-center font-bold mt-2 dark:text-gray-400">Software developer</h2>
+        </div>
+        <div class="absolute bottom-2 w-full flex justify-center">
+            <a href="#my-story" class="block text-gray-600 hover:text-gray-900 dark:text-gray-400 hover:dark:text-gray-100 p-4 transition duration-200">
+                <span class="sr-only">Go to my story</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 motion-safe:animate-bounce" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5" />
+                </svg>                              
+            </a>
+        </div>
+    </section>
+    <section id="my-story">
+        <div class="bg-gradient-to-b from-sky-500 to-sky-700 px-4 py-8 md:py-16">
+            <h2 class="text-3xl md:text-5xl text-white font-bold text-center">My story</h2>
+        </div>
+        <div class="max-w-2xl mx-auto p-4">
+            <p>
+                As you already know, my name is Patrick van 't Wout. I often abbreviate my name as 'Pvtw' on the web.
+                First I would like to tell you my development story.
+            </p>
+            <h3 class="mt-4 text-xl font-bold">Where it began...</h3>
+            <p class="mt-2">
+                My first ever development project was a small website from the browser game Neopets.
+                Every character has a page which you can customize; Basically only HTML but with a small trick allows CSS aswell.
+                Very quickly I learned how to create a simple webpage.
+                <strong>BUT</strong> It doesn't stop there. I wanted to learn more about web development.
+                So I searched the internet on how to make better websites and another language came across my attention.
+                The wonderful world of PHP. I always say I learned PHP when PHP and MySQL had the same version number; version 5.4.
+                I created a forum from scratch for a RuneScape clan I was in. I became a PHP master.
+            </p>
+            <h3 class="mt-4 text-xl font-bold">And now...</h3>
+            <p class="mt-2">
+                Currently I am taking a software development course. During the course I learn how to work in a scrum team with agile.
+                Also a few programming languages like JavaScript and C#. Creating projects with colleagues to learn from and to keep us busy.
+                Altho side-projects, like this website, are a better way to learn and more fun. Especially in the fast moving world of web development.
+            </p>
+            <h3 class="mt-4 text-xl font-bold">The future</h3>
+            <p class="mt-2">
+                I see myself working at a company building amazing websites or applications.
+                Learning everyday and be helpful to eachother. New technologies that help us moving forward.
+                Also that little error you get. Struggling to get it working and your colleague fixed it in 5 minutes...
+                Yes, it happends. And I want to be there!
+            </p>
+        </div>
+    </section>
+    <section id="skills" class="mt-8">
+        <div class="bg-gradient-to-b from-lime-500 to-lime-700 px-4 py-8 md:py-16">
+            <h2 class="text-3xl md:text-5xl text-white font-bold text-center">Skills</h2>
+        </div>
+        <div class="max-w-2xl grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 mx-auto p-4">
+            <div>
+                <h3 class="text-xl text-black dark:text-white font-bold">Programming languages</h3>
+                <ul class="grid gap-4 mt-4">
+                    <li>
+                        <span class="text-lg">PHP</span>
+                        <div class="border border-gray-600 dark:border-gray-400">
+                            <div class="w-[98%] h-4 bg-gradient-to-b from-lime-500 to-lime-700">
+                                <span class="sr-only">98%</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text-lg">C#</span>
+                        <div class="border border-gray-600 dark:border-gray-400">
+                            <div class="w-[95%] h-4 bg-gradient-to-b from-lime-500 to-lime-700">
+                                <span class="sr-only">95%</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text-lg">TypeScript</span>
+                        <div class="border border-gray-600 dark:border-gray-400">
+                            <div class="w-[90%] h-4 bg-gradient-to-b from-lime-500 to-lime-700">
+                                <span class="sr-only">90%</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text-lg">SQL</span>
+                        <div class="border border-gray-600 dark:border-gray-400">
+                            <div class="w-[90%] h-4 bg-gradient-to-b from-lime-500 to-lime-700">
+                                <span class="sr-only">90%</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="mt-4 md:mt-0">
+                <h3 class="text-xl text-black dark:text-white font-bold">Tools and frameworks</h3>
+                <ul class="grid gap-4 mt-4">
+                    <li>
+                        <span class="text-lg">Laravel</span>
+                        <div class="border border-gray-600 dark:border-gray-400">
+                            <div class="w-[95%] h-4 bg-gradient-to-b from-lime-500 to-lime-700">
+                                <span class="sr-only">95%</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text-lg">.NET</span>
+                        <div class="border border-gray-600 dark:border-gray-400">
+                            <div class="w-[95%] h-4 bg-gradient-to-b from-lime-500 to-lime-700">
+                                <span class="sr-only">95%</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text-lg">Angular</span>
+                        <div class="border border-gray-600 dark:border-gray-400">
+                            <div class="w-[90%] h-4 bg-gradient-to-b from-lime-500 to-lime-700">
+                                <span class="sr-only">90%</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text-lg">Tailwind CSS</span>
+                        <div class="border border-gray-600 dark:border-gray-400">
+                            <div class="w-[90%] h-4 bg-gradient-to-b from-lime-500 to-lime-700">
+                                <span class="sr-only">90%</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="md:col-span-2 mt-4">
+                <p>My skills are not limited to the languages and frameworks mentioned above. I do find those the most important to know.</p>
+            </div>
+        </div>
+    </section>
+    <section id="meet-the-bird" class="mt-8">
+        <div class="bg-gradient-to-b from-orange-500 to-orange-700 px-4 py-8 md:py-16">
+            <h2 class="text-3xl md:text-5xl text-white font-bold text-center">Meet the bird</h2>
+        </div>
+        <div class="p-4">
+            <div class="bg-white max-w-sm flex flex-col items-center justify-center mt-8 mx-auto p-4 rounded-lg shadow-lg dark:bg-gray-800">
+                <img src="/images/cockatiel.png" class="w-64 h-64 rounded-full border-4 border-orange-600 shadow-lg" alt="Cockatiel" loading="lazy">
+                <p class="mt-4 text-center">
+                    This is my little cockatiel. His name is Kuifie (small crest).
+                    He is a oldie, almost 12 years old. He still likes to fly around and play.
+                    If you are lucky, he will say 'hi' back. Or atleast trying to.
+                </p>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
