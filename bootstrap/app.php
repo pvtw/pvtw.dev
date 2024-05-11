62418e130629b127c801a3aa16b5a987d32b693b
pvtw <patrick@pvtw.dev>
2024-05-11 16:19:11 +0200

    Add EnsureUserIsAdmin middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Middleware\EnsureUserIsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -14,6 +15,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'admin' => EnsureUserIsAdmin::class,
+        ]);
+
         // If the user is not authenticated when visiting a protected location
         // Redirect to the 'auth.login' route (The default is 'login')
         $middleware->redirectGuestsTo(fn (Request $request) => route('auth.login'));
