ceef81b0939e48f0749335e0dc1b6b9f7e083d13
pvtw <patrick@pvtw.dev>
2024-05-13 18:52:58 +0200

    Fix link

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-minimal-layout>
     <div class="bg-primary w-full min-h-screen flex flex-col justify-center items-center p-4">
-        <a href="index.html" class="block w-32 h-32 outline-none focus-visible:ring focus-visible:ring-blue-500">
+        <a href="{{ route('home') }}" class="block w-32 h-32 outline-none focus-visible:ring focus-visible:ring-blue-500">
             <img src="/images/logo-128x128.png" alt="Logo">
         </a>
         <div class="w-full max-w-lg bg-gray-100 dark:bg-gray-900 mt-1 p-4 rounded-xl shadow">
