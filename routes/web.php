8a2c43e1f495ecde16818cdbaf3add28fce7701d
pvtw <patrick@pvtw.dev>
2024-06-01 12:09:34 +0200

    Remove admin pages

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\AboutController;
-use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LoginWithGitHubController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -25,16 +24,4 @@ Route::middleware(['guest'])->group(function (): void {
 
 Route::middleware(['auth'])->group(function (): void {
     Route::post('/auth/logout', LogoutController::class)->name('auth.logout');
-});
-
-Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function (): void {
-    Route::view('/', 'pages.admin.index')->name('index');
-    Route::controller(AdminProjectController::class)->name('projects.')->group(function (): void {
-        Route::get('/projects', 'index')->name('index');
-        Route::get('/projects/create', 'create')->name('create');
-        Route::post('/projects', 'store')->name('store');
-        Route::get('/project/{project}/edit', 'edit')->name('edit');
-        Route::match(['put', 'patch'], '/project/{project}', 'update')->name('update');
-        Route::delete('/project/{project}', 'destroy')->name('destroy');
-    });
 });
