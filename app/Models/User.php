4fe60d45bd733b1519d78b9020df01ac61e73952
pvtw <patrick@pvtw.dev>
2024-06-01 12:13:07 +0200

    Install Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-final class User extends Authenticatable
+final class User extends Authenticatable implements FilamentUser
 {
     use HasFactory;
     use Notifiable;
@@ -38,5 +41,10 @@ final class User extends Authenticatable
             'password' => 'hashed',
             'is_admin' => 'boolean',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->is_admin;
     }
 }
