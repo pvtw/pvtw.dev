41dedb4af0d480ed95cd82055289c1268938598a
pvtw <patrick@pvtw.dev>
2024-06-08 17:12:00 +0200

    Increase font size and font weight on post links

--- resources/views/pages/posts/index.blade.php
+++ resources/views/pages/posts/index.blade.php
@@ -5,7 +5,7 @@
             @foreach($posts as $post)
                 <x-card>
                     <x-link href="{{ route('posts.show', $post) }}" class="block" wire:navigate>
-                        <h2>{{ $post->title }}</h2>
+                        <h2 class="text-lg lg:text-xl font-bold">{{ $post->title }}</h2>
                     </x-link>
                     <time datetime="{{ $post->published_at->format('Y-m-d') }}" class="text-sm italic mt-4">{{ $post->published_at->format('F jS Y') }}</time>
                 </x-card>
