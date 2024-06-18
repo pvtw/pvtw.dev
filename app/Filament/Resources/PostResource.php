932509be111e08a985e438bfade8348827e505e5
pvtw <patrick@pvtw.dev>
2024-06-18 11:07:13 +0200

    Add admin view page for posts

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -63,6 +63,7 @@ final class PostResource extends Resource
 
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -84,6 +85,7 @@ final class PostResource extends Resource
         return [
             'index' => Pages\ListPosts::route('/'),
             'create' => Pages\CreatePost::route('/create'),
+            'view' => Pages\ViewPost::route('/{record}'),
             'edit' => Pages\EditPost::route('/{record}/edit'),
         ];
     }
---
+++ resources/views/filament/infolists/components/markdown-entry.blade.php
@@ -0,0 +1,5 @@
+<x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
+    <x-markdown>
+        {{ $getState() }}
+    </x-markdown>
+</x-dynamic-component>
