a32e1cacdce7f19955c5a2fa3bd62b18fc6724e3
pvtw <patrick@pvtw.dev>
2024-06-22 23:39:29 +0200

    Run Laravel Pint

--- app/Filament/Infolists/Components/MarkdownEntry.php
+++ app/Filament/Infolists/Components/MarkdownEntry.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Infolists\Components;
 
 use Filament\Infolists\Components\Entry;
 
-class MarkdownEntry extends Entry
+final class MarkdownEntry extends Entry
 {
     protected string $view = 'filament.infolists.components.markdown-entry';
 }
--- app/Filament/Resources/PostResource/Pages/ViewPost.php
+++ app/Filament/Resources/PostResource/Pages/ViewPost.php
@@ -1,15 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
 use App\Filament\Infolists\Components\MarkdownEntry;
-use Filament\Actions;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
 
-class ViewPost extends ViewRecord
+final class ViewPost extends ViewRecord
 {
     protected static string $resource = PostResource::class;
 
