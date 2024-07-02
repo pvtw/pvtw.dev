29c50394e4b0dc19765de4e01646b7ce2773da7a
pvtw <patrick@pvtw.dev>
2024-07-02 18:21:19 +0200

    Add account registration
    but comment the routes to not activate it yet

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+final class RegisterController extends Controller
+{
+    public function create(): View
+    {
+        return view('pages.auth.register');
+    }
+
+    public function store(RegisterRequest $request): RedirectResponse
+    {
+        $user = User::create($request->validated());
+
+        //event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect()->route('home');
+    }
+}
