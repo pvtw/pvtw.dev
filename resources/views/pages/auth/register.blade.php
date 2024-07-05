f22f80aa2c5ae2c161fab13e9ea8f84aa8ab172f
pvtw <patrick@pvtw.dev>
2024-07-05 15:58:52 +0200

    style

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -47,7 +47,7 @@
                         <button type="submit" class="bg-primary dark:bg-gray-100 text-gray-100 dark:text-gray-900 outline-none focus-visible:ring focus-visible:ring-blue-500 font-bold px-4 py-2 rounded shadow">Create</button>
                     </div>
                 </form>
-          </div>
+            </div>
         </div>
     </div>
 </x-minimal-layout>
