417c65ea1eafb16d4cf186cb355e50a82f7d0367
pvtw <patrick@pvtw.dev>
2024-07-14 19:06:03 +0200

    Styling my account menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,7 +110,7 @@
                     </div>
                 </div>
                 <nav id="primary-navigation" class="w-full sm:w-auto h-[calc(100vh-4rem)] sm:h-auto overflow-y-auto sm:overflow-y-visible sm:mr-2 p-2 sm:p-0" x-cloak x-show="open">
-                    <ul class="flex flex-col sm:flex-row gap-2">
+                    <ul class="flex flex-col sm:flex-row sm:items-center gap-2">
                         <li>
                             <a
                                 href="{{ route('posts.index') }}"
@@ -140,23 +140,23 @@
                         </li>
                         @auth
                             <li>
-                                <details id="myAccountMenu" class="md:relative" @click.away="document.getElementById('myAccountMenu').removeAttribute('open')">
-                                    <summary class="cursor-pointer block w-full md:w-12 h-12 flex justify-between md:justify-center items-center outline-none focus-visible:ring focus-visible:ring-blue-500">
-                                        <span class="md:sr-only block text-xl text-nowrap font-bold px-4 pt-2 pb-1 border-b-4 border-transparent hover:border-fuchsia-500 transition-colors duration-200">My Account</span>
+                                <details id="myAccountMenu" class="sm:relative border-t border-fuchsia-500 sm:border-none" @click.away="document.getElementById('myAccountMenu').removeAttribute('open')">
+                                    <summary class="cursor-pointer block w-full sm:w-12 h-12 flex justify-between sm:justify-center items-center pr-2 sm:p-0 outline-none focus-visible:ring focus-visible:ring-blue-500">
+                                        <span class="sm:sr-only block text-xl text-nowrap font-bold px-4 pt-2 pb-1 border-b-4 border-transparent hover:border-fuchsia-500 transition-colors duration-200">My Account</span>
                                         @if(null !== auth()->user()->avatar_url)
-                                            <img src="{{ auth()->user()->avatar_url }}" alt="{{ auth()->user()->name }}'s avatar" class="w-12 h-12 rounded-full">
+                                            <img src="{{ auth()->user()->avatar_url }}" alt="{{ auth()->user()->name }}'s avatar" class="w-8 h-8 rounded-full">
                                         @else
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8" aria-hidden="true">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                             </svg>
                                         @endif
                                     </summary>
-                                    <ul class="md:absolute md:top-12 md:right-0 bg-gray-100 dark:bg-gray-900 text-black dark:text-white p-2 rounded shadow">
+                                    <ul class="sm:absolute sm:top-12 sm:right-0 bg-gray-100 dark:bg-gray-900 text-black dark:text-white p-2 rounded shadow">
                                         <li>
                                             <form method="post" action="{{ route('logout') }}">
                                                 @csrf
 
-                                                <button type="submit" class="block w-full md:w-auto text-xl text-nowrap text-left font-bold px-4 pt-2 pb-1 border-b-4 border-transparent hover:border-fuchsia-500 transition-colors duration-200 outline-none focus-visible:ring focus-visible:ring-blue-500">Logout</button>
+                                                <button type="submit" class="block w-full sm:w-auto text-xl text-nowrap text-left font-bold px-4 pt-2 pb-1 border-b-4 border-transparent hover:border-fuchsia-500 transition-colors duration-200 outline-none focus-visible:ring focus-visible:ring-blue-500">Logout</button>
                                             </form>
                                         </li>
                                     </ul>
