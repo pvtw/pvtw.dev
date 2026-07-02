b3090fc6d18df2f710a12781ac71c3a4c764461e
pvtw <patrick@pvtw.dev>
2026-07-02 19:13:14 +0200

    Add types and remove unnecessary code

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -4,80 +4,31 @@ declare(strict_types=1);
 
 namespace App\Notifications;
 
-use Illuminate\Contracts\Routing\UrlGenerator;
-use Illuminate\Contracts\Mail\Mailable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Lang;
-use Closure;
+use SensitiveParameter;
 
 final class ResetPassword extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * The callback that should be used to create the reset password URL.
-     *
-     * @var (Closure(mixed, string): string)|null
-     */
-    public static $createUrlCallback;
-
-    /**
-     * The callback that should be used to build the mail message.
-     *
-     * @var (Closure(mixed, string):MailMessage|Mailable)|null
-     */
-    public static $toMailCallback;
-
-    /**
-     * Create a notification instance.
-     *
-     * @param  string  $token
-     */
     public function __construct(
-        /**
-         * The password reset token.
-         */
-        public $token
+        #[SensitiveParameter] public string $token,
     ) {
     }
 
-    /**
-     * Get the notification's channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array|string
-     */
-    public function via($notifiable): array
+    public function via(object $notifiable): array
     {
         return ['mail'];
     }
 
-    /**
-     * Build the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return MailMessage
-     */
-    public function toMail($notifiable)
+    public function toMail(object $notifiable): MailMessage
     {
-        if (self::$toMailCallback) {
-            return call_user_func(self::$toMailCallback, $notifiable, $this->token);
-        }
+        $url = $this->resetUrl($notifiable);
 
-        return $this->buildMailMessage($this->resetUrl($notifiable));
-    }
-
-    /**
-     * Get the reset password notification mail message for the given URL.
-     *
-     * @param  string  $url
-     * @return MailMessage
-     */
-    private function buildMailMessage($url)
-    {
         return new MailMessage()
             ->subject(Lang::get('Reset Password Notification'))
             ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
@@ -86,41 +37,11 @@ final class ResetPassword extends Notification implements ShouldQueue
             ->line(Lang::get('If you did not request a password reset, no further action is required.'));
     }
 
-    /**
-     * Get the reset URL for the given notifiable.
-     *
-     * @param  mixed  $notifiable
-     * @return string
-     */
-    private function resetUrl($notifiable): string|UrlGenerator
+    private function resetUrl(object $notifiable): string
     {
-        if (self::$createUrlCallback) {
-            return call_user_func(self::$createUrlCallback, $notifiable, $this->token);
-        }
-
         return url(route('password.reset', [
             'token' => $this->token,
             'email' => $notifiable->getEmailForPasswordReset(),
         ], false));
-    }
-
-    /**
-     * Set a callback that should be used when creating the reset password button URL.
-     *
-     * @param  Closure(mixed, string): string  $callback
-     */
-    public static function createUrlUsing($callback): void
-    {
-        self::$createUrlCallback = $callback;
-    }
-
-    /**
-     * Set a callback that should be used when building the notification mail message.
-     *
-     * @param Closure(mixed, string):((MailMessage|Mailable)) $callback
-     */
-    public static function toMailUsing($callback): void
-    {
-        self::$toMailCallback = $callback;
     }
 }
--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -12,62 +12,20 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\URL;
-use Closure;
 
 final class VerifyEmail extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * The callback that should be used to create the verify email URL.
-     *
-     * @var Closure|null
-     */
-    public static $createUrlCallback;
-
-    /**
-     * The callback that should be used to build the mail message.
-     *
-     * @var Closure|null
-     */
-    public static $toMailCallback;
-
-    /**
-     * Get the notification's channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array|string
-     */
-    public function via($notifiable): array
+    public function via(object $notifiable): array
     {
         return ['mail'];
     }
 
-    /**
-     * Build the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return MailMessage
-     */
-    public function toMail($notifiable)
+    public function toMail(object $notifiable): MailMessage
     {
-        $verificationUrl = $this->verificationUrl($notifiable);
+        $url = $this->verificationUrl($notifiable);
 
-        if (self::$toMailCallback) {
-            return call_user_func(self::$toMailCallback, $notifiable, $verificationUrl);
-        }
-
-        return $this->buildMailMessage($verificationUrl);
-    }
-
-    /**
-     * Get the verify email notification mail message for the given URL.
-     *
-     * @param  string  $url
-     * @return MailMessage
-     */
-    private function buildMailMessage($url)
-    {
         return new MailMessage()
             ->subject(Lang::get('Verify Email Address'))
             ->line(Lang::get('Please click the button below to verify your email address.'))
@@ -75,18 +33,8 @@ final class VerifyEmail extends Notification implements ShouldQueue
             ->line(Lang::get('If you did not create an account, no further action is required.'));
     }
 
-    /**
-     * Get the verification URL for the given notifiable.
-     *
-     * @param  mixed  $notifiable
-     * @return string
-     */
-    private function verificationUrl($notifiable)
+    private function verificationUrl(object $notifiable): string
     {
-        if (self::$createUrlCallback) {
-            return call_user_func(self::$createUrlCallback, $notifiable);
-        }
-
         return URL::temporarySignedRoute(
             'verification.verify',
             Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
@@ -95,25 +43,5 @@ final class VerifyEmail extends Notification implements ShouldQueue
                 'hash' => sha1((string) $notifiable->getEmailForVerification()),
             ]
         );
-    }
-
-    /**
-     * Set a callback that should be used when creating the email verification URL.
-     *
-     * @param  Closure  $callback
-     */
-    public static function createUrlUsing($callback): void
-    {
-        self::$createUrlCallback = $callback;
-    }
-
-    /**
-     * Set a callback that should be used when building the notification mail message.
-     *
-     * @param  Closure  $callback
-     */
-    public static function toMailUsing($callback): void
-    {
-        self::$toMailCallback = $callback;
     }
 }
